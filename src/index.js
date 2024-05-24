ab05b3d6c8ca0f3df31ddf060cca1843b4a44d76
Gianni Castellano <gianniccastellano@gmail.com>
2024-05-23 22:38:19 -0400

    ramen code challenge

--- src/index.js
+++ src/index.js
@@ -1,29 +1,59 @@
-// index.js
+//Global
+const baseUrl = "http://localhost:3000"
+const newRamenForm = document.getElementById('new-ramen')
+const ramenMenu = document.getElementById('ramen-menu')
 
-// Callbacks
-const handleClick = (ramen) => {
-  // Add code
-};
+document.addEventListener("DOMContentLoaded", function() {
+  getRamens()
+})
 
-const addSubmitListener = () => {
-  // Add code
+newRamenForm.addEventListener('submit', addSubmitListener)
+
+function getRamens() {
+  fetch(`${baseUrl}/ramens`)
+  .then(respnse => respnse.json())
+  .then(ramen => displayRamens(ramen))
 }
 
-const displayRamens = () => {
-  // Add code
-};
-
-const main = () => {
-  // Invoke displayRamens here
-  // Invoke addSubmitListener here
+function displayRamens (ramens) {
+  ramens.forEach(ramen => {
+    displayRamen(ramen)
+  })
 }
 
-main()
+function displayRamen(ramen) {
+  const ramenImgHolder = document.createElement('img')
+  ramenImgHolder.src = ramen.image
+  ramenMenu.append(ramenImgHolder)
+  ramenImgHolder.addEventListener('click', () => handleClick(ramen))
+}
 
-// Export functions for testing
-export {
-  displayRamens,
-  addSubmitListener,
-  handleClick,
-  main,
-};
+function handleClick(ramen) {
+  document.querySelector('.detail-image').src = ramen.image
+  document.querySelector('.name').textContent = ramen.name
+  document.querySelector('.restaurant').textContent = ramen.restaurant
+  document.querySelector('#rating-display').textContent = ramen.rating
+  document.querySelector('#comment-display').textContent = ramen.comment
+ }
+
+function addSubmitListener(event) {
+  event.preventDefault()
+  const newRamenItem = {
+    name: event.target.name.value,
+    restaurant: event.target.restaurant.value,
+    image: event.target.image.value,
+    rating: event.target.rating.value,
+    comment: event.target["new-comment"].value,
+  }
+
+  displayRamen(newRamenItem);
+ }
+
+// function main() {
+//   displayRamen()
+//   addSubmitListener()
+//  }
+
+// main()
+
+
